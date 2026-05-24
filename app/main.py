146890b06341f2f26ba7264535ac1eb52bee496c
Nazar Vovchuk <nazarvovchuc3002@gmail.com>
2026-05-24 21:32:33 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ def calculate_team_total_rating(players: list) -> int:
     return sum(player.get_rating() for player in players)
 
 
-def elves_concert(elves: list) -> None:
+def elves_concert(elves: list) -> str:
     for elf in elves:
         elf.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+def feast_of_the_dwarves(dwarves: list) -> str:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
--- app/players/player.py
+++ app/players/player.py
@@ -2,16 +2,14 @@ from abc import ABC, abstractmethod
 
 
 class Player(ABC):
-    players = []
 
     def __init__(self, nickname: str) -> None:
         self.nickname = nickname
-        Player.players.append(self)
 
     @abstractmethod
     def get_rating(self) -> int:
         pass
 
     @abstractmethod
-    def player_info(self) -> None:
+    def player_info(self) -> str:
         pass
